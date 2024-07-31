5669416fe945be41d455b1ba4e076577c28875ed
Coders Club <hi@codersclub.com>
2024-07-31 19:32:26 +0300

    added pyperclip and last changes in password generator.

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+def generate_password():
+  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
+  password_letters = [choice(letters) for i in range(randint(8, 10))]
+  password_numbers = [choice(numbers) for i in range(randint(2, 4))]
+  password_symbols = [choice(symbols) for i in range(randint(2, 4))]
+  password_list = password_letters + password_numbers + password_symbols
 
-password_letters = [random.choice(letters) for i in range(nr_letters)]
-password_numbers = [random.choice(numbers) for i in range(nr_numbers)]
-password_symbols = [random.choice(symbols) for i in range(nr_symbols)]
-password_list = password_letters + password_numbers + password_symbols
+  shuffle(password_list)
 
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
+  password = "".join(password_list)
+  password_entry.delete(0, END)
+  password_entry.insert(0, password)
+  pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def Save():
@@ -83,7 +79,7 @@ email_username_entry.insert(0, "tomtomi@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
 
 
